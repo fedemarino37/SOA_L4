fd4150ba13909ee9ee6bc9afab89e1f08c8c34d3
Federico Marino Dragoset <federico.emarino@mercadolibre.com>
2022-10-30 21:59:46 -0300

    getBattery logic refactored

--- app/src/main/java/com/example/TP2/repository/BatteryRepository.java
+++ app/src/main/java/com/example/TP2/repository/BatteryRepository.java
@@ -10,14 +10,6 @@ public class BatteryRepository {
     public BatteryRepository() {}
 
     public String getPercentage(Context ctx) {
-        IntentFilter ifilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
-        Intent batteryStatus = ctx.registerReceiver(null, ifilter);
 
-        int level = batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
-        int scale = batteryStatus.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
-
-        float batteryPct = level * 100 / (float)scale;
-
-        return Float.toString(batteryPct);
     }
 }
--- app/src/main/java/com/example/TP2/usecase/GetBatteryPercentage.java
+++ app/src/main/java/com/example/TP2/usecase/GetBatteryPercentage.java
@@ -5,16 +5,16 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.BatteryManager;
 
-import com.example.TP2.repository.BatteryRepository;
-
 public class GetBatteryPercentage {
-    BatteryRepository batteryRepository;
+    public String execute(Context ctx) {
+        IntentFilter ifilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        Intent batteryStatus = ctx.registerReceiver(null, ifilter);
 
-    public GetBatteryPercentage() {
-        batteryRepository = new BatteryRepository();
-    }
+        int level = batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+        int scale = batteryStatus.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
 
-    public String getPercentage(Context ctx) {
-        return batteryRepository.getPercentage(ctx);
+        float batteryPct = level * 100 / (float)scale;
+
+        return Float.toString(batteryPct);
     }
 }
